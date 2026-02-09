ed73ae3a1e97af69d4d4c59054dfc43b621b0e6d
Jägermeist <philipthorell@outlook.com>
2026-02-09 11:48:23 +0100

    Updated nvim to make all floating windows have a border

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -10,9 +10,19 @@ vim.opt.runtimepath:append("/opt/nvim/lib/nvim")
 
 vim.diagnostic.config({
 	virtual_text = true,
-  underline = true,
+	underline = true,
 	float = {
 		border = "rounded",
 		focusable = true,
 	},
 })
+
+local border = "rounded" -- Options: "single", "double", "shadow", "rounded"
+
+-- Override globally for LSP floating windows
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or border
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
