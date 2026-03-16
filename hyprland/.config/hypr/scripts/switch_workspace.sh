ffaf307d9971b9feb8a2e6745addec46381ca1f5
Jägermeist <philipthorell@outlook.com>
2026-03-16 18:43:11 +0100

    Updated hyprland config and scripts & waybar config because i switched ports on my monitors

--- hyprland/.config/hypr/scripts/switch_workspace.sh
+++ hyprland/.config/hypr/scripts/switch_workspace.sh
@@ -4,7 +4,11 @@ MONITOR=$(hyprctl activeworkspace -j | jq -r '.monitor')
 
 # If the monitor is the secondary monitor, add 10 to the workspace number
 if [ "$MONITOR" == "DP-1" ]; then
-    hyprctl dispatch workspace $(( $1 + 10 ))
-else
     hyprctl dispatch workspace $1
+elif [ "$MONITOR" == "DP-3" ]; then
+    hyprctl dispatch workspace $(( $1 + 10 ))
+elif [ "$MONITOR" == "eDP-1" ]; then
+    hyprctl dispatch workspace $(( $1 + 20 ))
+else
+    hyprctl dispatch workspace $(( $1 + 30 ))
 fi
